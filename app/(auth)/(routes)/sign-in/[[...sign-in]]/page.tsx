49cd7bdcf3ceb72f4ff596ae7ddeedc5ac017bfb
PixelPioneer2003 <codinglit2908@gmail.com>
2025-06-22 19:03:46 +0530

    Initial commit for EduSphere LMS

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+// app/sign-up/[[...sign-up]]/page.tsx
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+      <SignIn />
+  );
+}
---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+// app/sign-up/[[...sign-up]]/page.tsx
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+      <SignUp />
+  );
+}
