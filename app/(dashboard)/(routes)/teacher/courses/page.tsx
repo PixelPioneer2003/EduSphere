5d4cf6da720c972c1e13a126676587a412dc5348
PixelPioneer2003 <codinglit2908@gmail.com>
2025-06-25 15:32:13 +0530

    search functionality implemented

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,32 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
-  return (
+import { db } from "@/lib/db";
+
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+
+const CoursesPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/search");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return ( 
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
-  );
-};
-
+   );
+}
+ 
 export default CoursesPage;
--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-
+// This hook is used to debounce a value, delaying its update until after a specified delay period.
+//wait for some time before making search request in the  database
 export function useDebounce<T>(value: T, delay?:number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
